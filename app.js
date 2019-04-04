183fad4ec499bb46f48e0f7ccfdaaa03740ed5c5
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2019-04-04 12:46:01 -0400

    nothing really

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "uri": process.env.MONGODB_URI
+    uri: process.env.MONGODB_URI,
+    project: process.env.PROJECT_ID
   });
 });
 
