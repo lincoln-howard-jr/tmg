9b2ef28e8e5e65da07514db0bb8c907f60f984d7
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2019-04-06 14:25:54 -0400

    decrypt bug fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 const aws = require ('aws-sdk');
-let kms = new aws.KMS ();
+let kms = new aws.KMS ({region: 'us-east-1'});
 
 const decryptEnv = (env) => {
   return new Promise ((resolve, reject) => {
     kms.decrypt ({CiphertextBlob: new Buffer (process.env [env], 'base64')}, (err, data) => {
       if (err) return reject (err);
-      resolve (data);
+      resolve (data.Plaintext.toString ());
     });
   });
 }
