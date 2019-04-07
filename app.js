7721e9feac674d37add13e92437a3a5d9bfe1385
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2019-04-07 18:19:05 -0400

    update env

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let kms = new aws.KMS ({region: 'us-east-1'});
 const decryptEnv = (env) => {
   return new Promise ((resolve, reject) => {
     kms.decrypt ({CiphertextBlob: new Buffer (process.env [env])}, (err, data) => {
+      console.log (err, data);
       if (err) return reject (err);
       resolve (data.Plaintext.toString ());
     });
