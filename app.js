a0bf5944f74a92d241f53e9104f8a964d5103489
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2019-04-08 12:20:49 -0400

    base64 encoding

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let kms = new aws.KMS ({region: 'us-east-1'});
 
 const decryptEnv = (env) => {
   return new Promise ((resolve, reject) => {
-    kms.decrypt ({CiphertextBlob: new Buffer (process.env [env])}, (err, data) => {
+    kms.decrypt ({CiphertextBlob: new Buffer (process.env [env], 'base64')}, (err, data) => {
       console.log (err, data);
       if (err) return reject (err);
       resolve (data.Plaintext.toString ());
