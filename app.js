a798b8be4c6093ef505838be9654d082c4af1b9b
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2019-04-29 12:20:45 -0400

    cors enable credentials

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ const enableCors = (req, res, next) => {
   res.set ('Access-Control-Allow-Origin', '*');
   res.set ('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
   res.set ('Access-Control-Allow-Headers', '*')
+  res.set ('ACcess-Control-Allow-Credentials', true)
   next ();
 }
 
